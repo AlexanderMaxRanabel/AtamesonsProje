061dd10a5654b6d960420699f2b02d7e0586b93f
AlexanderMaxRanabel <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2022-12-11 23:33:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     // Write C++ code here
   string basla,gec,gec1,gec2,sk1,sk2,sk3;
-  int puan;
+  int puan=0;
 
   cout<<"Başlamak İçin BAŞLA yazınız"<<endl;
   cin>>basla;
@@ -14,38 +14,44 @@ int main() {
   cout<<"Tutumluluk,Sorumluluk,Çalışkanlık"<<endl;
   cin>>sk1;
   if(sk1=="Tutumluluk")
-    puan=puan++;
+    puan = puan++;
   cout<<puan<<endl;
   cout<<"Geçmek için Geç yazınız"<<endl;
   cin>>gec;
-  if(sk1=="Sorumluluk"||"Çalışkanlık)
-    puan=puan--;
+  if(sk1=="Sorumluluk"||"Çalışkanlık")
+    puan = puan--;
   cout<<"YALNIŞ DOĞRU CEVAP TUTUMLULUK"<<endl;
   cout<<puan<<endl;
+
+
+  if(gec=="Geç")
+      cout<<"Ak Akçe Kara gün içindir"<<endl;
+  cout<<"Fedakarlık,Vatanseverlik,Tutumluluk"<<endl;
+  cin>>sk2;
+  cout<<"Doğru!"<<endl;
+  puan = puan++;
+  cout<<puan<<endl;
+  cout<<"Geçmek için Geç yazınız"<<endl;
   cin>>gec2;
+  if(sk2=="Vatanseverlik"||"Fedakarlık")
+    puan = puan--;
+  cout<<"YALNIŞ DOĞRU CEVAP TUTUMLULUK"<<endl;
+  cout<<puan<<endl;
 
   if(gec2=="Geç")
-    cout<<"Ak Akçe Kara gün içindir"<<endl;
-  cout<<"Fedakarlık,Vatanseverlil,Tutumluluk"<<endl;
-  cin>>sk2;
-  if(sk2=="Tutumluluk")
-    cout<<"Doğru!"<<endl;
-  puan = puan++;
-  cout<<puan++<<endl;
-  cout<<"Geçmek için Geç yazınız"<<endl;
-  cin>>gec1;
-  if(gec1=="Geç")
-    cout<<"Komşu Komşunun Külüne Muhtaçtır"<<endl;
-    cout<<"Yardımlaşma Çalışkanlık Hoş Görü"<<endl;
-    cin>>sk3;
-    if(sk3=="Yardımlaşma")
-      //I dont know what ım doing by any bit
+      cout<<"Komşu Komşunun Külüne Muhtaçtır"<<endl;
+  cout<<"Yardımlaşma Çalışkanlık Hoş Görü"<<endl;
+  cin>>sk3;
+  if(sk3=="Yardımlaşma")
+    //I dont know what ım doing by any bit lol
       cout<<"DOĞRU"<<endl;
     puan = puan++;
+    cout<<puan<<endl;
+    cout<<"TEBRİKLER TAMAMLADINIZ"<<endl;
     if(sk3=="Çalışkanlık"||"Hoş Görü")
-    puan=puan--;
-  cout<<"YALNIŞ DOĞRU CEVAP TUTUMLULUK"<<endl;
-  cout<<puan<<endl; 
+      puan=puan--;
+    cout<<"YALNIŞ DOĞRU CEVAP YARDIMLAŞMA"<<endl;
+    cout<<puan<<endl;
 
-    return 0;
+  return 0;
 }
